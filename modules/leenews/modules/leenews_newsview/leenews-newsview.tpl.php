d7e1cad94aaf313f3c9ab4eb97a6998e68f31387
letuong <git@letuong.unfuddle.com>
2010-03-28 20:20:41 +0700

    Add simple test module:

--- modules/leenews/modules/leenews_newsview/leenews-newsview.tpl.php
+++ modules/leenews/modules/leenews_newsview/leenews-newsview.tpl.php
@@ -9,7 +9,12 @@
 				<div><?php print $row['title']; ?></div>	
 				<?php if ($first_news == 0): ?>
 					<div class="clearfix news-summary">
-						<?php print $row['field_news_thumbnail_fid']; ?>
+						
+						<?php 
+						if (isset($row['field_news_thumbnail_fid'])) {
+							print $row['field_news_thumbnail_fid'];
+						} 
+						?>
 						<p>
 							<?php print $row['teaser']; ?>
 						</p>	
